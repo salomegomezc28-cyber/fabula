d120026fcdbced70493423ba2c6d0d361e7ec699
salomegomezc28-cyber <salomegomezc28@gmail.com>
2026-02-26 11:08:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 import base64
 
 st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+image = Image.open('rinrin.jpg')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
@@ -19,13 +19,61 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.subheader("rin rin renacuajo")
+st.write('El hijo de rana, Rinrín renacuajo
+Salió esta mañana muy tieso y muy majo
+Con pantalón corto, corbata a la moda
+Sombrero encintado y chupa de boda.
+-¡Muchacho, no salgas!- le grita mamá
+pero él hace un gesto y orondo se va.
+Halló en el camino, a un ratón vecino
+Y le dijo: -¡amigo!- venga usted conmigo,
+Visitemos juntos a doña ratona
+Y habrá francachela y habrá comilona.
+A poco llegaron, y avanza ratón,
+Estírase el cuello, coge el aldabón,
+Da dos o tres golpes, preguntan: ¿quién es?
+-Yo doña ratona, beso a usted los pies
+¿Está usted en casa? -Sí señor sí estoy,
+y celebro mucho ver a ustedes hoy;
+estaba en mi oficio, hilando algodón,
+pero eso no importa; bienvenidos son.
+Se hicieron la venia, se dieron la mano,
+Y dice Ratico, que es más veterano :
+Mi amigo el de verde rabia de calor,
+Démele cerveza, hágame el favor.
+Y en tanto que el pillo consume la jarra
+Mandó la señora traer la guitarra
+Y a renacuajo le pide que cante
+Versitos alegres, tonada elegante.
+-¡Ay! de mil amores lo hiciera, señora,
+pero es imposible darle gusto ahora,
+que tengo el gaznate más seco que estopa
+y me aprieta mucho esta nueva ropa.
+-Lo siento infinito, responde tía rata,
+aflójese un poco chaleco y corbata,
+y yo mientras tanto les voy a cantar
+una cancioncita muy particular.
+Mas estando en esta brillante función
+De baile y cerveza, guitarra y canción,
+La gata y sus gatos salvan el umbral,
+Y vuélvese aquello el juicio final
+Doña gata vieja trinchó por la oreja
+Al niño Ratico maullándole: ¡Hola!
+Y los niños gatos a la vieja rata
+Uno por la pata y otro por la cola
+Don Renacuajito mirando este asalto
+Tomó su sombrero, dio un tremendo salto
+Y abriendo la puerta con mano y narices,
+Se fue dando a todos noches muy felices
+Y siguió saltando tan alto y aprisa,
+Que perdió el sombrero, rasgó la camisa,
+se coló en la boca de un pato tragón
+y éste se lo embucha de un solo estirón
+Y así concluyeron, uno, dos y tres
+Ratón y Ratona, y el Rana después;
+Los gatos comieron y el pato cenó,
+¡y mamá Ranita solita quedó!'
         
         )
            
